4dc52bf66fbc521b0685be3c5576f0bac223d0ca
noumanlumensoft <noumanaamer100@gmail.com>
2022-10-06 12:42:06 +0500

    register commit

---
+++ controllers/auth.js
@@ -0,0 +1,18 @@
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
+const User = require('../models/User')
+
+const register = async (req, res) => {
+    const user = await User.create({...req.body})
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({user:{name: user.name}, token})
+}
+
+const login = async (req, res) => {
+    res.send('Login User')
+}
+
+module.exports = {
+    register, 
+    login
+}
